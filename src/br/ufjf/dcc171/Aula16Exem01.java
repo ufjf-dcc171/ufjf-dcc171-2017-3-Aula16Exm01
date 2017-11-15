a0cfff19b571cdda8a5c1a54d60a27532d5aeaad
Igor Knop <igorknop@gmail.com>
2017-11-14 22:00:33 -0200

    Adiciona laço

--- src/br/ufjf/dcc171/Aula16Exem01.java
+++ src/br/ufjf/dcc171/Aula16Exem01.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -16,26 +17,28 @@ public class Aula16Exem01 {
             String driverUrl = "jdbc:derby://localhost:1527/dcc171-2013-3";
             Connection conexao = DriverManager.getConnection(driverUrl, "usuario", "senha");
             PreparedStatement operacaoInsere = conexao.prepareStatement("INSERT INTO produto(nome, qtd, atualizado) VALUES(?,?,CURRENT_TIMESTAMP)");
-            PreparedStatement operacaoListar  = conexao.prepareStatement("SELECT nome, qtd FROM produto WHERE qtd > ?");
-            operacaoInsere.clearParameters();
-            operacaoInsere.setString(1, "Produto X");
-            operacaoInsere.setInt(2, 10);
-            operacaoInsere.executeUpdate();
-            
+            PreparedStatement operacaoListar = conexao.prepareStatement("SELECT nome, qtd FROM produto WHERE qtd > ? ORDER BY qtd DESC");
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++) {
+                operacaoInsere.clearParameters();
+                operacaoInsere.setString(1, "Produto " + rnd.nextInt(100));
+                operacaoInsere.setInt(2, rnd.nextInt(10) + 1);
+                operacaoInsere.executeUpdate();
+            }
             operacaoListar.clearParameters();
             operacaoListar.setInt(1, 0);
             ResultSet resultado = operacaoListar.executeQuery();
-            while(resultado.next()){
+            while (resultado.next()) {
                 System.out.println(
                         resultado.getString(1)
-                        +"\t\t"
-                        +resultado.getInt(2)
+                        + "\t\t"
+                        + resultado.getInt(2)
                 );
             }
-            
+
         } catch (Exception ex) {
             Logger.getLogger(Aula16Exem01.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
 }
